654a08973312a418b4ac36bde08c24e5c63bfc77
OLUSOLA70 <adeniranolusola91@gmail.com>
2024-11-14 14:21:51 +0100

    Initial commit for Nutrition Meter

---
+++ src/App.js
@@ -0,0 +1,13 @@
+//App.js
+import React from "react";
+import NutritionMeter from "./Nutritionmeter";
+
+function App() {
+  return (
+    <div className="bg-gray-100 min-h-screen">
+      <NutritionMeter />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,14 @@
+// index.js
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css"; // Import Tailwind CSS and any global styles
+import App from "./App"; // Import the main App component
+import './Nutri.css';
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
